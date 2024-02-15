075cc1aa6985a0cad3429afee429362846f56fbd
ivarprudnikov <ivar.prudnikov@gmail.com>
2024-02-15 21:44:02 +0000

    add users store

--- internal/storage/users.go
+++ internal/storage/users.go
@@ -1,1 +1,52 @@
 package storage
+
+import (
+	"sync"
+	"time"
+)
+
+type UserStore struct {
+	users sync.Map
+	salt  string
+}
+
+func NewUserStore(salt string) *UserStore {
+	return &UserStore{users: sync.Map{}, salt: salt}
+}
+
+func (u *UserStore) AddUser(username string, password string) (User, error) {
+	usr := NewUser(username, password)
+	u.users.Store(usr.Username, usr)
+	return usr, nil
+}
+
+func (u *UserStore) GetUser(username string, password string) (*User, error) {
+	if v, ok := u.users.Load(username); ok {
+		if usr, ok := v.(User); ok {
+			// TODO use salted hash
+			if usr.Password == HashText(password) {
+				return &User{
+					Username: usr.Username,
+					Created:  usr.Created,
+				}, nil
+			}
+		}
+	}
+	return nil, nil
+}
+
+type User struct {
+	Id       string    `json:"id"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Created  time.Time `json:"created"`
+}
+
+func NewUser(username string, password string) User {
+	return User{
+		Username: username,
+		// TODO use salt for hashing to mitigate rainbow attacks
+		Password: HashText(password),
+		Created:  time.Now(),
+	}
+}
