ae0fb00f2f7aa18ff6cf8d4dbb40093a4e3a2602
ivarprudnikov <ivar.prudnikov@gmail.com>
2024-04-27 18:36:44 +0100

    trim current build dir from the binary

--- deployments/azure.fn.deploy.sh
+++ deployments/azure.fn.deploy.sh
@@ -7,7 +7,7 @@ FN_NAME="secret-share"
 # Remove prior versions
 rm -f bin/server package.zip
 # Build the binary
-GOOS=linux GOARCH=amd64 go build -o bin/server .
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -o bin/server .
 # Prepare function package
 zip -r package . --include "bin/*" "func-match-all-config/*" host.json
 # Deploy the package
